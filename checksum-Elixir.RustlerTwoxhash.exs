bbd01ee0433731500aceca5746ae80b1bd606670
George Lima <georgelima11@hotmail.com>
2022-08-01 10:11:28 -0300

    chore: add checksums

---
+++ checksum-Elixir.RustlerTwoxhash.exs
@@ -0,0 +1,10 @@
+%{
+  "libtwoxhash-v0.0.1-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:cdd691843e417c5e6ba0b3a0a58ce8b965748ce9bd475eb5d483f7c1013114eb",
+  "libtwoxhash-v0.0.1-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:9dee8bfe06044f7f6c7eb05fdf32e9c399ddc3ed0c78ba214c81e949079be193",
+  "libtwoxhash-v0.0.1-nif-2.16-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:631219c8fa6a5acce8fa9fbda1fb042d3485dd9ee67bb3bd076e2696d607006b",
+  "libtwoxhash-v0.0.1-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:fb1960dc9e6fc46964c21ee05526393727aa957d3087f403aa2bc1bc8b37e8fa",
+  "libtwoxhash-v0.0.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:35f0cf41e4bffdf5bc60862a44d6bd3843b8a7f535a790913e18ca800ab1c980",
+  "libtwoxhash-v0.0.1-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:2d66473b1712feb6f1e44bcdee3aadda5e77f0c22594abf35c55794ef5bccd07",
+  "twoxhash-v0.0.1-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:d6d0ab8c5a57211fde7b15c616b339d9ba20db1ace3745895f9bd5750d5a2a81",
+  "twoxhash-v0.0.1-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:8ddbcbb2e3a4a754de250088766d56f580ab3e4056751411770040747caa6b24",
+}
